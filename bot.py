939bd8ba48d8cd0de1ac0be40afa4e4195963f63
saransh121 <34244742+saransh121@users.noreply.github.com>
2024-12-24 13:30:22 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -256,10 +256,8 @@ def should_trade(symbol, model, scaler, data, balance):
         position_size = (POSITION_SIZE_PERCENT * balance) / current_price
         position_size = validate_position_size(symbol, position_size, current_price)
         atr = data['ATR'].iloc[-1]
-        buy_threshold = 1
-        #1.002 + (atr / current_price * 0.05)  # Adjust by 10% of ATR
-        sell_threshold = 1
-        #0.998 - (atr / current_price * 0.05)
+        buy_threshold = 1.002 + (atr / current_price * 0.05)  # Adjust by 10% of ATR
+        sell_threshold = 0.998 - (atr / current_price * 0.05)
 
         crossover_signal = detect_crossover(data)
 
@@ -273,7 +271,7 @@ def should_trade(symbol, model, scaler, data, balance):
               and (crossover_signal == 'buy' )
                 #  or ((data['MA_10'].iloc[-1] > data['MA_30'].iloc[-1]) 
                 #  and (data['MACD'].iloc[-1] > data['Signal'].iloc[-1]) 
-                and (30 < data['RSI'].iloc[-1] < 50) 
+                #and (30 < data['RSI'].iloc[-1] < 50) 
                 #and (data['MACD'].iloc[-1] > 0)
         ):
             return 'buy', position_size
@@ -284,7 +282,7 @@ def should_trade(symbol, model, scaler, data, balance):
               and (crossover_signal == 'sell' )
                 #  or ((data['MA_10'].iloc[-1] < data['MA_30'].iloc[-1]) 
                 # and (data['MACD'].iloc[-1] < data['Signal'].iloc[-1])
-               and (data['RSI'].iloc[-1] > 65)
+                #and (data['RSI'].iloc[-1] > 65)
                 # and (data['MACD'].iloc[-1] < 0)
         ):
             return 'sell', position_size
