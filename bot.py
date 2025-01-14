1285b2269f34bf61ecc76a6162103d86d4b93dc2
saransh121 <34244742+saransh121@users.noreply.github.com>
2025-01-14 07:45:52 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -268,8 +268,8 @@ def should_trade(symbol, model, scaler, data, balance):
         # Remove the proximity condition for buy and sell
         # Buy Condition
         if (
-             (predicted_price > (current_price * buy_threshold))
-              and (crossover_signal == 'buy' )
+             ((predicted_price > (current_price * buy_threshold))
+              or (crossover_signal == 'buy' ))
                 #  or ((data['MA_10'].iloc[-1] > data['MA_30'].iloc[-1]) 
                 #  and (data['MACD'].iloc[-1] > data['Signal'].iloc[-1]) 
                 #and (30 < data['RSI'].iloc[-1] < 50) 
@@ -279,8 +279,8 @@ def should_trade(symbol, model, scaler, data, balance):
 
         # Sell Condition
         elif (
-            (predicted_price < (current_price * sell_threshold))
-                # (crossover_signal == 'sell' )
+            ((predicted_price < (current_price * sell_threshold))
+                or (crossover_signal == 'sell' ))
                 #  or ((data['MA_10'].iloc[-1] < data['MA_30'].iloc[-1]) 
                 # and (data['MACD'].iloc[-1] < data['Signal'].iloc[-1])
                 #and (data['RSI'].iloc[-1] > 65)
